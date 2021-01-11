2a4a6cbef36c203cdd973c064e346b27cbf2a4ac
RedyAu <fodor.benedek2001@gmail.com>
2021-01-11 23:21:39 +0100

    first commit

---
+++ bin/pdf_gen.dart
@@ -0,0 +1,13 @@
+import 'package:image/image.dart';
+import 'package:diff_image/diff_image.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+
+void main() {
+  print('PDF-GEN');
+  Directory vidDir = Directory('_Source Videos');
+  Directory tempDir = Directory('TEMP');
+
+  //Get directory system
+  
+}
