89da44a736c7c4fdebd97c8ae66a17638f310406
RedyAu <fodor.benedek2001@gmail.com>
2021-01-13 16:59:48 +0100

    finalising interface

---
+++ bin/default_config.dart
@@ -0,0 +1,32 @@
+String defaultConfig = """Config file for PDF-GEN
+Written by RedyAu in 2021
+ONLY EDIT THE VALUES AFTER THE COLONS! IF YOU MESSED UP THE FILE, JUST DELETE IT.
+
+-- EXTRACTING FRAMES --
+- Extract every Nth frame: 20
+1 means extract every single frame.
+
+- Extracted frames extension: .bmp
+Recommended: .bmp
+.jpg file size is smaller, but introduces noise and makes the next step slower and less accurate.
+Warning: Using .bmp, for a longer video, you may need multiple GBs of hard disk space!
+
+
+-- MARKING USEFUL SLIDES --
+- Percentage treshold for new slide: 0.1
+Ranges 0-100. Can be any fraction. If no pixels are the same, will be 100.
+Recommended values:
+0.1 for .bmp extraction setting (may be decreased further if there are smaller animations on screen)
+0.6-1.0 for .jpg extraction setting (may have duplicates and/or skip slides!)
+
+- Intro transition lenth in frames: 0
+Counts actually extracted frames, take your setting value at "extract every Nth frame" into account!
+
+- Transition length between slides in frames: 1
+As the script goes trough the extracted frames, if it finds a difference beyond the treshold, it will jump ahead by this amount, mark that frame as a useful slide, and continue checking from there.
+Counts actually extracted frames, take your setting value at "extract every Nth frame" into account!
+
+
+--- EXPORTING TO PDF ---
+- JPG quality of slides: 60
+.bmp frames get converted to .jpg before getting added to the exported .pdf files. Set the quality in percentages here. 100 is lossless (but bigger).""";
--- bin/pdf_gen.dart
+++ bin/pdf_gen.dart
@@ -5,29 +5,32 @@ import 'package:image/image.dart';
 import 'package:diff_image/diff_image.dart';
 import 'dart:io';
 import 'package:path/path.dart';
+import 'default_config.dart';
+import 'package:http/http.dart' as http;
 
-//! CONFIG
-final int extractEveryNthFrame = 20;
-final double chooserFidelity = 0.1;
-final int chooserTransitionLength = 2;
-final int chooserBeginTransitionLength = 2;
-final int jpgQuality = 60;
+//Configurables
+int extractEveryNthFrame;
+String extractedFramesExtension = "";
+double chooserFidelity;
+int chooserTransitionLength;
+int chooserBeginTransitionLength;
+int jpgQuality;
 
 //Globals
-final String extractedFramesExtension = ".bmp";
 final List<String> videoExtensions = ['.mp4', '.avi', '.mov'];
 
 final Directory rootDir = Directory(r'PDF-GEN');
 final Directory vidDir = Directory(r'PDF-GEN\_Source Videos');
 final Directory tempDir = Directory(r'PDF-GEN\TEMP');
+final File configFile = File(r'PDF-GEN\config.txt');
 List<SourceVideo> vids = List<SourceVideo>();
 
 void main() async {
   print('PDF-GEN\n\nInitializing...');
-  if (!vidDir.existsSync())
-    new Directory(vidDir.path).createSync(recursive: true);
-  if (!tempDir.existsSync())
-    new Directory(tempDir.path).createSync(recursive: true);
+  if (await initialize()) {
+    //if true, terminate
+    return;
+  }
 
   print('Getting source videos...\n\n');
   // Get source videos
@@ -54,20 +57,76 @@ void main() async {
       continue;
     }
 
+    clearTemp();
+
     print(
-        '1. Extracting frames... This may take much time and hard disk space.');
-    await runFFMPEG(vid);
+        '1. Extracting frames... This may take a while, and consume much disk space.');
+    String ffmpegOutput = await runFFMPEG(vid);
 
     print('2. Choosing unique frames...');
-    var keepFrames = await chooseFrames();
+    var keepFrames = await chooseFrames(ffmpegOutput);
 
     print('3. Exporting pdf...');
     await createPdf(keepFrames, withoutExtension(vid.file.path) + ".pdf");
 
-    for (var entity in tempDir.listSync()) entity.deleteSync();
     print('------------------- Done! -------------------\n\n');
     index++;
   }
+
+  clearTemp();
+
+  print('Goodbye!');
+}
+
+void clearTemp() {
+  for (var entity in tempDir
+      .listSync()
+      .where((item) => extension(item.path) != ".exe")) entity.deleteSync();
+}
+
+Future<bool> initialize() async {
+  if (!vidDir.existsSync()) {
+    new Directory(vidDir.path).createSync(recursive: true);
+    print(
+        "Created folder for source videos. Place videos in any folder structure inside and relaunch the program.");
+  }
+  if (!configFile.existsSync()) {
+    new File(configFile.path).writeAsStringSync(defaultConfig);
+    print(
+        "Created config file with default settings. Please edit them and relaunch the program.");
+    return true;
+  } else {
+    List<String> configLines = configFile.readAsLinesSync();
+
+    extractEveryNthFrame =
+        int.parse(getConfigValue(configLines, "- Extract every Nth frame: "));
+
+    extractedFramesExtension =
+        getConfigValue(configLines, "- Extracted frames extension: ");
+
+    chooserFidelity = double.parse(
+        getConfigValue(configLines, "- Percentage treshold for new slide: "));
+
+    chooserBeginTransitionLength = int.parse(
+        getConfigValue(configLines, "- Intro transition lenth in frames: "));
+
+    chooserTransitionLength = int.parse(getConfigValue(
+        configLines, "- Transition length between slides in frames: "));
+
+    jpgQuality =
+        int.parse(getConfigValue(configLines, "- JPG quality of slides: "));
+  }
+  if (!tempDir.existsSync())
+    new Directory(tempDir.path).createSync(recursive: true);
+
+  return false;
+}
+
+String getConfigValue(List<String> configLines, String key) {
+  //Calling this a key is a bit of a stretch but hey, this isn't open source (oh wait)
+  return configLines
+      .firstWhere((element) => element.startsWith(key))
+      .split(": ")[1];
 }
 
 void createPdf(List<Frame> frames, String path) async {
@@ -90,15 +149,20 @@ void createPdf(List<Frame> frames, String path) async {
   await file.writeAsBytesSync(await pdf.save());
 }
 
-Future<List<Frame>> chooseFrames() async {
+Future<List<Frame>> chooseFrames(String ffmpegOutput) async {
   print(' 2.1. Getting file list...');
   List<Frame> frames = List<Frame>();
   for (File file in tempDir.listSync().where((element) =>
       element is File && extension(element.path) == extractedFramesExtension))
     frames.add(Frame(file, int.parse(basenameWithoutExtension(file.path))));
   frames.sort((a, b) => a.index.compareTo(b.index));
+  if (frames.length == 0) {
+    print("Error! There were no frames extracted. This was the FFMPEG log:\n" +
+        ffmpegOutput.toString());
+  }
 
-  print(' 2.2. Comparing frames... This may take a while.');
+  print(
+      " 2.2. Comparing frames... This may take a while.\nTIP: The program checks for existing .pdf files, you can stop it and continue next time from the video file you stopped it at (extracted frames and difference checking progress don't get saved.)");
   List<Frame> toKeep = List<Frame>();
   int index = 1 + chooserBeginTransitionLength;
   toKeep.add(frames[index]);
@@ -129,21 +193,28 @@ Future<List<Frame>> chooseFrames() async {
   return toKeep;
 }
 
-void runFFMPEG(SourceVideo vid) async {
-  await Process.run(
-      'ffmpeg',
-      [
-        '-i',
-        canonicalize(vid.file.path),
-        '-vf',
-        'select=not(mod(n\\,$extractEveryNthFrame))',
-        '-vsync',
-        'vfr',
-        '%06d$extractedFramesExtension'
-      ],
-      //runInShell: true,
-      workingDirectory: canonicalize(tempDir.path),
-      stderrEncoding: utf8);
+Future<String> runFFMPEG(SourceVideo vid) async {
+  try {
+    return Process.runSync(
+            'ffmpeg',
+            [
+              '-i',
+              canonicalize(vid.file.path),
+              '-vf',
+              'select=not(mod(n\\,$extractEveryNthFrame))',
+              '-vsync',
+              'vfr',
+              '%06d$extractedFramesExtension'
+            ],
+            //runInShell: true,
+            workingDirectory: canonicalize(tempDir.path),
+            stderrEncoding: utf8)
+        .stderr;
+  } catch (e) {
+    print(
+        "A konvertálás nem sikerült! Telepítve van az ffmpeg?\nHa nincs: https://www.wikihow.com/Install-FFmpeg-on-Windows");
+    exit(1);
+  }
 }
 
 class SourceVideo {
